8c5749581a18198aa506b86ce564d5bcd8aff27c
sanketmedhepawar <sanketmedhe311@gmail.com>
2025-07-22 09:36:11 +0000

    Added IAM Users using Terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"  
+}
+
+variable "names" {
+  default = ["hari", "om", "rohit"]
+}
+
+resource "aws_iam_user" "my_iam_users" {
+  for_each = toset(var.names)
+  name     = each.value
+}
